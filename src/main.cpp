289a96a462ea009548d190bce63eee6b261d0940
Ameer <ameerhaziqosman@gmail.com>
2022-12-02 08:20:23 +0100

    add

--- src/main.cpp
+++ src/main.cpp
@@ -5,30 +5,31 @@
 #include "filefunctions.h"
 #include "PubSubClient.h"
 #include <ArduinoJson.h>
+#include <M5StickCPlus.h>
 
 int connection_counter = 0;
 
 void setup()
 {
-
+  M5.begin();
   // open serial connection (console output)
   Serial.begin(115200);
   // start SPIFFS
   SPIFFS.begin(true);
-
-  
 
   // display device info on config page
   WiFiSettings.hostname = device_ID;
   WiFiSettings.heading("Sensor ID:");
   WiFiSettings.heading(device_ID);
 
-  
   // reset connection counter
   connection_counter = 0;
 
   // try to connect to WiFi with a timeout of 30 seconds - launch portal if connection fails
-  WiFiSettings.connect(true, 30);
+  if(WiFiSettings.connect(true, 30))
+  {
+    Lcd.printf("");
+  }
 
   // get time
   configTime(0, 0, ntpServer);
@@ -42,5 +43,5 @@ void loop()
 {
   // put your main code here, to run repeatedly:
   // run update server
-    AsyncElegantOTA.loop();
+  AsyncElegantOTA.loop();
 }
