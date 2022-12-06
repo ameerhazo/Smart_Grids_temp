9fdf25e38870788f65baef52c8f6ae0bfd195a4c
Ameer <ameerhaziqosman@gmail.com>
2022-12-06 18:10:27 +0100

    added code

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,10 @@
 #include "filefunctions.h"
 #include "PubSubClient.h"
 #include <ArduinoJson.h>
-#include <M5StickCPlus.h>
+// #include <M5StickCPlus.h>
+
+#define M5_LOW 1
+#define M5_HIGH 0
 
 unsigned long FADE_PEDIOD = 5000; // fade time is 3 seconds
 
@@ -13,8 +16,67 @@ unsigned long fadeUPStartTime;
 unsigned long fadeDownStartTime;
 long brightness;
 
+bool buttonActive = false;
+bool longPressActive = false;
+
 int connection_counter = 0;
 int red_LED = 10;
+
+int impulse_pin = 13; //define interrupt pin to 2
+int impulse_counter = 0;
+volatile int state = LOW; // To make sure variables shared between an ISR
+
+// void test_button_press()
+// {
+//   if (M5.BtnA.pressedFor(3000))
+//   {
+//     WiFiSettings.portal();
+//   }
+//   // if (digitalRead(red_Button) == LOW)
+//   // {
+
+//   //   if (buttonActive == false)
+//   //   {
+
+//   //     buttonActive = true;
+//   //     buttonTimer = millis();
+//   //   }
+
+//   //   if ((millis() - buttonTimer > longPressTime) && (longPressActive == false))
+//   //   {
+//   //     if (paused)
+//   //     {
+//   //       longPressActive = true;
+//   //       Serial.println("resume pressed!");
+//   //       resume_pressed = true;
+//   //       beep(2);
+//   //     }
+//   //   }
+//   // }
+//   // else
+//   // {
+
+//   //   if (buttonActive == true)
+//   //   {
+
+//   //     if (longPressActive == true)
+//   //     {
+
+//   //       longPressActive = false;
+//   //     }
+//   //     else
+//   //     {
+
+//   //       Serial.println("short pressed!");
+//   //       pause_pressed = true;
+//   //       beep(1);
+//   //     }
+
+//   //     buttonActive = false;
+//   //   }
+//   // }
+// }
+
 
 void configure_LED(void)
 {
@@ -22,6 +84,18 @@ void configure_LED(void)
   digitalWrite(red_LED, HIGH);
   fadeUPStartTime = millis();
   fadeDownStartTime = millis();
+}
+
+void increment_impulse(void)
+{
+  // ISR 
+  impulse_counter++;
+}
+
+void configure_impulse_pin(void)
+{
+  pinMode(impulse_pin, INPUT_PULLUP);
+  // attachInterrupt(digitalPinToInterrupt(impulse_pin), increment_impulse, FALLING);
 }
 
 void fade_LED_up(void)
@@ -39,70 +113,88 @@ void fade_LED_up(void)
   }
 }
 
-void connected_AP(void)
-{
-  if (WiFi.status() == WL_CONNECTED)
-  {
-    // Serial.println("Not connected to AP, start soft AP and fade LED");
-    fade_LED_up();
-    // fade_LED_down();
-  }
-}
+// void connected_AP(void)
+// {
+//   if (WiFi.status() == WL_CONNECTED)
+//   {
+//     // Serial.println("Not connected to AP, start soft AP and fade LED");
+//     fade_LED_up();
+//     // fade_LED_down();
+//   }
+// }
+
+// void on_Success(void)
+// {
+//   M5.Lcd.fillScreen(WHITE);
+//   delay(500);
+//   M5.Lcd.fillScreen(RED);
+//   delay(500);
+//   M5.Lcd.fillScreen(GREEN);
+//   delay(500);
+//   M5.Lcd.fillScreen(BLUE);
+//   delay(500);
+//   M5.Lcd.fillScreen(BLACK);
+//   delay(500);
+//   M5.Lcd.setCursor(0, 55);
+//   String IP_String = WiFi.localIP().toString();
+//   M5.Lcd.printf("%s", IP_String.c_str());
+// }
+
+// void on_Failure(void)
+// {
+//   digitalWrite(red_LED, M5_HIGH);
+// }
 
 void setup()
 {
-  M5.begin();
+  // M5.begin();
 
-  M5.Lcd.setTextColor(WHITE);
-  M5.Lcd.setTextSize(3);
-  M5.Lcd.setRotation(1);
+  Serial.begin(115200);
 
-  configure_LED();
+  // M5.Lcd.setTextColor(WHITE);
+  // M5.Lcd.setTextSize(3);
+  // M5.Lcd.setRotation(1);
+
+  // configure_LED();
   // start SPIFFS
-  SPIFFS.begin(true);
+  // SPIFFS.begin(true);
+
+  configure_impulse_pin();
+
+  // WiFiSettings.onSuccess = []()
+  // { on_Success(); };
+  // WiFiSettings.onFailure = []()
+  // { on_Failure(); };
+  // WiFiSettings.onWaitLoop = []() {}; // delay 30 ms
+  // WiFiSettings.onPortalWaitLoop = []() {};
 
   // display device info on config page
-  WiFiSettings.hostname = device_ID;
-  WiFiSettings.heading("Sensor ID:");
-  WiFiSettings.heading(device_ID);
+  // WiFiSettings.hostname = device_ID;
+  // WiFiSettings.heading("Sensor ID:");
+  // WiFiSettings.heading(device_ID);
 
-  // reset connection counter
-  connection_counter = 0;
+  // // reset connection counter
+  // connection_counter = 0;
 
-  // try to connect to WiFi with a timeout of 30 seconds - launch portal if connection fails
-  if (WiFiSettings.connect(true, 30))
-  {
-    M5.Lcd.fillScreen(WHITE);
-    delay(500);
-    M5.Lcd.fillScreen(RED);
-    delay(500);
-    M5.Lcd.fillScreen(GREEN);
-    delay(500);
-    M5.Lcd.fillScreen(BLUE);
-    delay(500);
-    M5.Lcd.fillScreen(BLACK);
-    delay(500);
-    M5.Lcd.setCursor(0, 55);
-    String IP_String = WiFi.localIP().toString();
-    M5.Lcd.printf("%s", IP_String.c_str());
-  }
-  else
-  {
-    digitalWrite(red_LED, LOW);
-  }
+  // // try to connect to WiFi with a timeout of 30 seconds - launch portal if connection fails
+  // WiFiSettings.connect(true, 30);
 
   // get time
-  configTime(0, 0, ntpServer);
+  // configTime(0, 0, ntpServer);
 
   // start update-functionality
-  AsyncElegantOTA.begin(&server);
-  server.begin();
+  // AsyncElegantOTA.begin(&server);
+  // server.begin();
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
   // run update server
-  connected_AP();
-  AsyncElegantOTA.loop();
+  // test_button_press();
+  // connected_AP();
+  // AsyncElegantOTA.loop();
+  // Serial.printf("counter = %d\n", impulse_counter);
+  Serial.println(digitalRead(impulse_pin));
+  delay(50);
 }
