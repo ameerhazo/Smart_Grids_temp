c85ca15de2002ed44d024a5847d3088626c43d6c
Ameer <ameerhaziqosman@gmail.com>
2022-12-08 17:38:35 +0100

    added mqtt stuff

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,29 @@
 #define M5_LOW 1
 #define M5_HIGH 0
 
+WiFiClient Client;
+
+PubSubClient MQTT_Client;
+
+// MQTT sml topic string
+String mainTopic = "things/";
+// MQTT SUITE topic string
+String metadata_topic = String("things/" + String(device_ID));
+// MQTT SUITE lwt topic string
+String lwt_Topic = String(metadata_topic + "/lwt");
+// TD_topic
+String TD_topic;
+
+/*                   MQTT property topic example                    */
+// things/EHz_Server_ID/properties/property_name (SML data name)
+//
+
+// MQTT last will testament message
+String lwt_message = String("{ \"status\": \"offline\"}");
+
+// boolean to check for connected to broker
+bool connected_tobroker = false;
+
 unsigned long FADE_PEDIOD = 5000; // fade time is 3 seconds
 
 unsigned long fadeUPStartTime;
@@ -27,6 +50,8 @@ int red_LED = 10;
 int impulse_pin = 13; // define interrupt pin to 2
 int impulse_counter = 0;
 volatile int state = LOW; // To make sure variables shared between an ISR
+
+void reconnect();
 
 void test_button_press()
 {
@@ -215,11 +240,15 @@ void setup()
     String device_ID_dummy = WiFiSettings.string("Custom device name", "device_name001");
   }
 
+  WiFiSettings.heading("CoAP Configuration");
+  bool dev_uses_CoAP_dummy = WiFiSettings.checkbox("Send data via CoAP");
+
   WiFiSettings.heading("MQTT Configuration");
+  bool dev_uses_MQTT_dummy = WiFiSettings.checkbox("Send data via MQTT");
   String mqtt_brokeraddress_dummy = WiFiSettings.string("MQTT broker address", "mqtt.eclipse.org");
   bool checkbox_mqtt_auth_dummy = WiFiSettings.checkbox("MQTT broker authentification", false);
-  String mqtt_brokerauth_user = WiFiSettings.string("Username", "username");
-  String mqtt_brokerauth_pass = WiFiSettings.string("Password", "password");
+  String mqtt_brokerauth_user = WiFiSettings.string("MQTT Broker Username", "username");
+  String mqtt_brokerauth_pass = WiFiSettings.string("MQTT Broker Password", "password");
 
   // WiFiSettings.heading("Device mode");
   // bool checkbox_Gas_dummy = WiFiSettings.checkbox("Gas", false);
@@ -228,9 +257,8 @@ void setup()
 
   WiFiSettings.heading("Gas Meter Configuration");
   String meter_Number = WiFiSettings.string("Meter number", "Gas_Meter-001");
-  String meter_Reading = WiFiSettings.string("Meter reading","12345.000");
+  String meter_Reading = WiFiSettings.string("Meter reading", "12345.000");
   // int meter_Reading = WiFiSettings.integer("Meter reading", );
-
 
   // reset connection counter
   connection_counter = 0;
@@ -242,20 +270,55 @@ void setup()
   // try to connect to WiFi with a timeout of 30 seconds - launch portal if connection fails
   WiFiSettings.connect(true, 30);
 
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setCursor(0, 55);
+  M5.Lcd.println("CONNECTED");
+  delay(1000);
+
+  //
   use_custom_Name = use_custom_name_Dummy;
+  mqttbroker_Address = mqtt_brokeraddress_dummy;
+  mqtt_needs_Auth = checkbox_mqtt_auth_dummy;
+  mqttauth_Username = mqtt_brokerauth_user;
+  mqttauth_Password = mqtt_brokerauth_pass;
+  current_meter_Reading = meter_Reading.toDouble();
 
   if (use_custom_Name)
   {
-    device_Name = custom_device_ID_Dummy;
+    if (custom_device_ID_Dummy != "")
+    {
+      device_Name = custom_device_ID_Dummy;
+      WiFiSettings.hostname = device_Name;
+    }
   }
   else
   {
     device_Name = "";
   }
 
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setCursor(0, 20);
+  M5.Lcd.println("TIME SYNC");
+  delay(1000);
+
+  configTime(3600, 0, ntpServer);
+
+  M5.Lcd.setCursor(0, 55);
+  M5.Lcd.println("SUCCESS");
+  delay(1000);
+
   // checkbox_Gas = checkbox_Gas_dummy;
   // checkbox_Strom = checkbox_Strom_dummy;
   // checkbox_Wasser = checkbox_Wasser_dummy;
+
+  if (mqttbroker_Address != "")
+  {
+  }
+  else
+  {
+    Serial.println("User forgot to set MQTT broker address, restarting portal");
+    WiFiSettings.portal();
+  }
 
   M5.Lcd.fillScreen(WHITE);
   delay(500);
@@ -272,7 +335,6 @@ void setup()
   M5.Lcd.printf("%s", IP_String.c_str());
 
   // get time
-  configTime(0, 0, ntpServer);
 
   // start update-functionality
   AsyncElegantOTA.begin(&server);
@@ -283,6 +345,12 @@ void loop()
 {
   // put your main code here, to run repeatedly:
   // run update server
+  // try to reconnect to the MQTT broker
+  reconnect();
+
+  // perform background tasks for the communication using MQTT
+  MQTT_Client.loop();
+
   M5.update();
   test_button_press();
   connected_AP();
@@ -290,4 +358,31 @@ void loop()
   // Serial.printf("counter = %d\n", impulse_counter);
   // Serial.println(digitalRead(impulse_pin));
   // delay(50);
+}
+
+void reconnect()
+{
+  Serial.print("Attempting MQTT connection...");
+
+  // Loop until we're reconnected
+  while (!MQTT_Client.connected())
+  {
+    int retries = 0;
+
+    if (retries < 5)
+    {
+      
+      if (mqtt_needs_Auth)
+      {
+        connected_tobroker = MQTT_Client.connect(, mqttauth_Username.c_str(), mqttauth_Password.c_str(), lwt_Topic.c_str(), 1, true, lwt_message.c_str());
+      }
+      else
+      {
+        connected_tobroker = MQTT_Client.connect(device_ID, lwt_Topic.c_str(), 1, true, lwt_message.c_str());
+      }
+
+      Serial.println("Retries exceeded, restarting device");
+      ESP.restart();
+    }
+  }
 }
